8555ca4f4056ef11f6647bee1a4919c813d3d6d5
avneet6 <143005707+avneet6@users.noreply.github.com>
2025-01-06 20:28:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AddNewMember from './components/AddNewMember';
+import Table from './components/Table';
+import EditMember from './components/EditMember';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<div><Navbar/><Table /></div>} />
+          <Route path="/add-new-member" element={<AddNewMember />} />
+          <Route path="/edit-member/:id" element={<EditMember />} />
+        </Routes>
+        <ToastContainer />
+      </div>
+    </Router>
     </div>
   );
-}
+};
 
 export default App;
